7507e57c551e90c6c87ff89840c54f971801a667
aakashDeveloper <ahanda205@gmail.com>
2019-08-29 22:00:58 +0530

    A

---
+++ javascript/first.js
@@ -0,0 +1,148 @@
+String
+Number 
+Boolean
+null
+undefined
+
+var city = "amsterdam"
+var city2 = 'london'
+var city3 = 'innsburg"
+var marks = "798"
+var mymarks = 90;
+var answer = "true";
+var output = false;
+var average = 8978.878
+
+var a = 10
+var b = 20
+
+
+var a = 10
+var b = 20
+undefined
+a+b
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+
+** + operter with number will be addition 
+** + operator with string will be concatination
+
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+Not a number
+
+
+String + String = String
+String + Number = String
+Number + Number = Number
+Number + String = String
+
+d+' '+e
+
+V8
+
+1 + 1 + "1"  =  "21"
+"1" + 1 + 1  =  "111"
+"1"+"1"+1    =  "111"
+
+true+true 
+
+
+
+
+truthy
+< 1, true 
+
+falsy
+>1, false, null, undefined
+
+
+
+true == 1
+false == 0
+
+true+true 
+2
+true+true
+2
+true+false
+1
+false+false
+0
+
+"a"+true+true
+"atruetrue"
+true+true+"a"
+"2a"
+false+"a"
+"falsea"
+"a"+false+false
+"afalsefalse"
+false+false+"a"
+"0a"
+false+false+1
+1
+
+boolean+string = string
+string+boolean = string
+boolean+boolean = number
+number+boolean = number
+
+
+10/2 => Divide
+5
+10%2 => Modules
+0
+10%3
+1
+10%4
+2
+10%5
+0
+10%6
+4
+10/6
+1.6666666666666667
+
+3%2
+1
+2%3
+2
+4%3
+1
+3%4
+3
+5%6
+5
+
+console.log()
+alert()
+prompt()
+confirm()
+
+
+
+console.log("hiii")
+VM1265:1 hiii
+undefined
+alert('success')
+undefined
+confirm('Do you want to leave')
+true
+confirm('Do you want to leave')
+false
+prompt('What is your name')
+"Aakash"
+prompt('What is your age')
+"25"
+prompt('What is your age')
